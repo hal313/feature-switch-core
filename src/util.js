fd35e0c630e449867f373518e6ad3260f3cd5dc7
John Ghidiu <john-github@rmdashrf.org>
2021-03-10 16:47:56 -0500

    Added mergeDeep

--- src/util.js
+++ src/util.js
@@ -9,3 +9,30 @@ export const isArray = Array.isArray;
 export const isObject = value => isDefined(value) && 'object' === typeof value;
 export const isObjectStrict = value => isObject(value) && !isArray(value);
 export const isFunction = value => 'function' === typeof value;
+
+export const mergeDeep = (target, ...sources) => {
+    // https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge
+    if (!sources.length) {
+        return target;
+    }
+    const source = sources.shift();
+
+    if (isObject(target) && isObject(source)) {
+        for (const key in source) {
+            if (isObject(source[key])) {
+                if (!target[key]) {
+                    Object.assign(target, {
+                        [key]: {}
+                    });
+                }
+                mergeDeep(target[key], source[key]);
+            } else {
+                Object.assign(target, {
+                    [key]: source[key]
+                });
+            }
+        }
+    }
+
+    return mergeDeep(target, ...sources);
+};
